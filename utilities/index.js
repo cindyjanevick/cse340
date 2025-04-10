8fc97524ede06fcc2368b23cf274e6693d0f282d
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-04-11 03:45:24 +0900

    add buildItemDetail View

--- utilities/index.js
+++ utilities/index.js
@@ -124,6 +124,81 @@ Util.buildItemListing = async function (data) {
   return listingHTML;
 };
 
+/* **************************************
+ * Build the inventory item detail view
+ ************************************** */
+Util.buildItemDetailView = function (item) {
+  if (!item) {
+    return '<p class="notice">Vehicle details not found.</p>';
+  }
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+
+  const milesFormatter = new Intl.NumberFormat("en-US", {
+    maximumFractionDigits: 0
+  });
+
+  let detailView = `
+    <div class="vehicle-detail">
+      <h1>${item.inv_make} ${item.inv_model}</h1>
+      <div>
+        <img src="${item.inv_image}" alt="Image of ${item.inv_make} ${item.inv_model}">
+      </div>
+      <div>
+        <p><strong>Price:</strong> $${formatter.format(item.inv_price)}</p>
+        <p><strong>Year:</strong> ${item.inv_year}</p>
+        <p><strong>Make:</strong> ${item.inv_make}</p>
+        <p><strong>Model:</strong> ${item.inv_model}</p>
+        <p><strong>Mileage:</strong> ${milesFormatter.format(item.inv_miles)}</p>
+        <p><strong>Description:</strong> ${item.inv_description}</p>
+      </div>
+    </div>
+  `;
+  console.log(item)
+  return detailView;
+};
+
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications()
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>'
+  classificationList += "<option value=''>Choose a Classification</option>"
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"'
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected "
+    }
+    classificationList += ">" + row.classification_name + "</option>"
+  })
+  classificationList += "</select>"
+  return classificationList
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* ****************************************
  * Middleware For Handling Errors
  * Wrap other function in this for 
